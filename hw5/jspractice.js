201e3a8397762fa4ddc8b106d8dfb5e1f8e14397
Sarah Kim <jiyunie13@gmail.com>
2014-04-17 23:54:48 -0700

    HW 1-6

---
+++ hw5/jspractice.js
@@ -0,0 +1,66 @@
+
+// EXERCISE 1
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	// Fill in your code here.
+}
+
+function multiply(a, b) {
+	// Fill in your code here.
+}
+
+function divide(a, b){
+	// Fill in your code here.
+}
+
+// EXERCISE 2
+
+// 1. YOUR ANSWER HERE
+// 2. YOUR ANSWER HERE
+// 3. YOUR ANSWER HERE
+
+
+// EXERCISE 3
+
+/*
+	CONVERT THE FOLLOWING LOGIC INTO JAVASCRIPT IF-ELSE STATEMENTS:
+	"“If there’s enough room for CS70, then I will enroll in CS70. Otherwise, I will see if CS61B is taught by Hilfinger. If CS61B is taught by Hilfinger, then I will avoid 61B and enroll in EE42. If not, CS 61B is not taught by Hilfinger, then I will enroll in 61B.”
+*/
+
+// YOUR CODE HERE
+
+
+// EXERCISE 4
+
+/* 
+	CONVERT THE FOLLOWING LOGIC INTO JAVASCRIPT WHILE LOOP STATEMENT AND FOR LOOP STATEMENT
+	John can drink up to 10 shots. If he drinks more than 10 shots, then he will throw up. As such, John will keep taking shots until he exactly reaches his threshold of 10 shots. Once he’s done drinking, he will shout out the number of shots he had and will pass out.
+	
+	Homework problem credit: Rachit Nanda
+*/
+
+var SHOTS_THRESHOLD = 10;
+var shots_taken = 0;
+
+// Complete while loop version of the logic here
+while () {
+	
+}
+
+console.log(shots_taken + "shots !!!");
+
+// -------------------------------------------------------
+
+var SHOTS_THRESHOLD = 10;
+var shots_taken = 0;
+
+// Complete for loop version of the logic here
+
+for () {
+	
+}
+
+console.log(shots_taken + "shots !!!");
